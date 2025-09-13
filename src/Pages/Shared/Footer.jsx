76e586e157388ee5f27ab4b65322a5a0c7e7b13e
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2025-09-13 18:01:18 +0600

    Refactor code structure for improved readability and maintainability

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,33 +1,55 @@
-
+import { Link } from "react-router-dom";
+import facebook from "../../assets/icons/facebook.svg";
+import insta from "../../assets/icons/insta.svg";
+import twitter from "../../assets/icons/twitter.svg";
+import youtube from "../../assets/icons/youtube.svg";
+import location from "../../assets/icons/location.svg";
+import email from "../../assets/icons/email.svg";
+import call from "../../assets/icons/call.svg";
 
 const Footer = () => {
-  
   return (
-    <footer className="bg-white md:py-16 py-7 container mx-auto">
+    <footer className="bg-white md:pt-16 pt-7 container mx-auto">
       <div className="px-4">
-        
         <div className="grid grid-cols-2 md:grid-cols-4 gap-8 mb-12">
           <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center mb-12 space-y-6 lg:space-y-0">
-          <div className="space-y-3">
-            <div className="text-2xl font-bold text-orange-600 tracking-tight">
-              SimFrii.com
+            <div className="space-y-3">
+              <div className="text-2xl font-bold text-orange-600 tracking-tight">
+                SimFrii.com
+              </div>
+              <p className="text-gray-600 max-w-xs">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Possimus, odio iure quasi, ab, consequatur velita reiciendis
+                itaque quaerat{" "}
+              </p>
+              <div className="flex space-x-4">
+                <Link to="/">
+                  <img src={facebook} alt="SimFrii Logo" />
+                </Link>
+                <Link to="/">
+                  <img src={twitter} alt="SimFrii Logo" />
+                </Link>
+                <Link to="/">
+                  <img src={insta} alt="SimFrii Logo" />
+                </Link>
+                <Link to="/">
+                  <img src={youtube} alt="SimFrii Logo" />
+                </Link>
+              </div>
             </div>
-            <p className="text-gray-600 max-w-xs">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
-              odio iure quasi, ab, consequatur velita reiciendis itaque quaerat{" "}
-            </p>
           </div>
-        </div>
 
           <div>
-            <h3 className="text-gray-900 font-semibold mb-4">Legal</h3>
+            <h3 className="text-gray-900 font-semibold mb-4">
+              Popular Countries
+            </h3>
             <ul className="space-y-3">
               <li>
                 <a
                   href="#"
                   className="text-gray-600 hover:text-[#8272ED] transition-colors"
                 >
-                  Terms
+                  United States
                 </a>
               </li>
               <li>
@@ -35,7 +57,7 @@ const Footer = () => {
                   href="#"
                   className="text-gray-600 hover:text-[#8272ED] transition-colors"
                 >
-                  Privacy
+                  London
                 </a>
               </li>
               <li>
@@ -43,21 +65,37 @@ const Footer = () => {
                   href="#"
                   className="text-gray-600 hover:text-[#8272ED] transition-colors"
                 >
-                  Help Center
+                  Canada
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-600 hover:text-[#8272ED] transition-colors"
+                >
+                  Australia
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-600 hover:text-[#8272ED] transition-colors"
+                >
+                  Germany
                 </a>
               </li>
             </ul>
           </div>
 
           <div>
-            <h3 className="text-gray-900 font-semibold mb-4">Providers</h3>
+            <h3 className="text-gray-900 font-semibold mb-4">About Us</h3>
             <ul className="space-y-3">
               <li>
                 <a
                   href="#"
                   className="text-gray-600 hover:text-purple-700 transition-colors"
                 >
-                  Join Now
+                  Contact Us
                 </a>
               </li>
               <li>
@@ -65,7 +103,7 @@ const Footer = () => {
                   href="#"
                   className="text-gray-600 hover:text-[#8272ED] transition-colors"
                 >
-                  Dashboard
+                  Blog
                 </a>
               </li>
               <li>
@@ -73,63 +111,58 @@ const Footer = () => {
                   href="#"
                   className="text-gray-600 hover:text-[#8272ED] transition-colors"
                 >
-                  Payments
+                  FAQ
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="text-gray-600 hover:text-[#8272ED] transition-colors"
+                >
+                  Help
                 </a>
               </li>
             </ul>
           </div>
 
           <div>
-            <h3 className="text-gray-900 font-semibold mb-4">Follow Us</h3>
+            <h3 className="text-gray-900 font-semibold mb-4">Contact</h3>
             <ul className="space-y-3">
-              <li>
+              <li className="flex items-center space-x-3">
+                <img src={location} alt="" />
                 <a
                   href="#"
                   className="text-gray-600 hover:text-[#8272ED] transition-colors"
                 >
-                  Facebook
+                  44 Danwers, NY City, USA, 70-102
                 </a>
               </li>
-              <li>
+              <li className="flex items-center space-x-3">
+                <img src={email} alt="" />
                 <a
-                  href="#"
+                  href="https://hr-akash23.web.app"
                   className="text-gray-600 hover:text-[#8272ED] transition-colors"
                 >
-                  Linkedin
+                  simfrii.contact@gmail.com
                 </a>
               </li>
-              <li>
+              <li className="flex items-center space-x-3">
+                <img src={call} alt="" />
                 <a
-                  href="#"
+                  href="https://hr-akash23.web.app"
                   className="text-gray-600 hover:text-[#8272ED] transition-colors"
                 >
-                  You Tube
+                  91+585-656-658
                 </a>
               </li>
             </ul>
           </div>
         </div>
 
-        <div className="border-t border-gray-200 pt-7">
-          <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
-            <div className="flex items-center space-x-4">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Stripe_Logo%2C_revised_2016.svg"
-                alt="Stripe"
-                className="h-6"
-              />
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Paypal_2014_logo.png"
-                alt="PayPal"
-                className="h-6"
-              />
-              <div className="text-gray-600 font-semibold text-sm">Klarna</div>
-            </div>
-
-            <div className="text-gray-500 text-sm">
-              © 2025 <span className="text-[#8272ED]">Evola</span>. All rights
-              reserved.
-            </div>
+        <div className="border-t border-gray-200 py-6">
+          <div className="text-gray-500 text-sm text-center">
+            © 2025 <span className="text-[#aa6320]">Akash</span>. All rights
+            reserved.
           </div>
         </div>
       </div>
