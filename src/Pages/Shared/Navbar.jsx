fbfd4877e6cd5dd6c368b4f214e30459893fefaa
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2025-09-20 14:24:25 +0600

    fixed

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import SupportModal from "../Support/supportModal";
+import SupportModal from "../Support/SupportModal";
 import ChatModal from "../Support/ChatModal";
 
 const Navbar = () => {
